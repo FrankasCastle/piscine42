0dc51fe814a54f547aa64bfcc0f44db988e98da8
Frankas <79930094+FrankasCastle@users.noreply.github.com>
2026-02-26 16:35:24 -0300

    Add files via upload

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucsanto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 16:04:27 by lucsanto          #+#    #+#             */
+/*   Updated: 2026/02/26 16:15:03 by lucsanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+{
+	int	counter;
+	counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		write(1, &str[counter], 1);
+		counter ++;
+	}
+}
+
+void	main(void)
+{
+	minha_string = "test";
+	ft_putstr(minha_string);
+}
